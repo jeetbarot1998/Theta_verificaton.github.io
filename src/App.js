fe43df0b4a8064783d97e7e0c162e732fce45902
Jeet Barot <jeetbarot1998@gmail.com>
2022-02-21 15:10:27 +0530

    URL CHANGES2

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
   };
 
   connectToThetaDropViaRedirect = async () => {
-    const hostUri = 'https://theta-verification.netlify.app/';
+    const hostUri = 'https://www.google.com/';
     this.thetaDrop.connectViaRedirect(AppId, hostUri);
   };
 
